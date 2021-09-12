d81cf2426fbaf1a85c97252923597de5d9d05c5d
kikeztw <kikeztw@gmail.com>
2021-09-12 18:29:02 -0400

    setup project

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+
+mongoose.connect(process.env.DATA_BASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
 app.use(cors())
 app.use(express.static('public'))
